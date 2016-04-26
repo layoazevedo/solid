a425dee72cad774d6b845dfb897445a6fd62894c
Layo Demetrio <layodemetrio@catho.com.br>
2016-04-26 14:46:52 -0300

    Implementado phpunit

---
+++ Estudo/Test/SingleResponsabilityTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LayoAzevedo\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class SingleResponsabilityTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function inicial()
+    {
+        $this->assertFalse(false);
+    }
+}
